8e8f814f27c40fa50132ff31942cbce158168527
akulbaeva <aiymbubu.kulbaeva@iaau.edu.kg>
2019-12-11 09:51:23 +0600

    Done

--- js/index.js
+++ js/index.js
@@ -22,3 +22,28 @@ $("#close-sidebar").click(function () {
 $("#show-sidebar").click(function () {
     $(".page-wrapper").addClass("toggled");
 });
+
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function email() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text("");
+
+    if (validateEmail(email)) {
+        $result.html('Correct &#10004;');
+        $result.css("color", "black");
+    } else {
+        $result.html('Incorrect &#10008;');
+        $result.css("color", "red");
+    }
+    return false;
+}
+
+$("#email").on("change", email);
+
